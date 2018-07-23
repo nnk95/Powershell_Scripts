a7f06d023678514570bb140df47b9f50c7b3cd00
nnk95 <39042433+nnk95@users.noreply.github.com>
2018-07-23 17:59:34 +0800

    v0.2
    Added custom windows logging to verify checking and times.

--- conn_test_TaskScheduler.ps1
+++ conn_test_TaskScheduler.ps1
@@ -5,14 +5,21 @@ $pingtestvmx = Test-Connection 12.0.0.5 -count 1 -quiet
 if(!$pingtest10x)
 {
     Restart-NetAdapter -Name "10*"
+    Write-EventLog -LogName NetworkRestartLogs -Source netrslog -Message "net 10x adapter restarted." -EventId 2 -EntryType Warning
 }
 
 if(!$pingtest192x)
 {
     Restart-NetAdapter -Name "192*"
+    Write-EventLog -LogName NetworkRestartLogs -Source netrslog -Message "net 192x adapter restarted." -EventId 2 -EntryType Warning
 }
 
 if(!$pingtestvmx)
 {
     Restart-NetAdapter -Name "VM*"
+    Write-EventLog -LogName NetworkRestartLogs -Source netrslog -Message "net VMx adapter restarted." -EventId 2 -EntryType Warning
+}
+elseif($pingtest10x,$pingtest192x,$pingtestvmx)
+{
+    Write-EventLog -LogName NetworkRestartLogs -Source netrslog -Message "No issues detected." -EventId 1 -EntryType Information
 }
